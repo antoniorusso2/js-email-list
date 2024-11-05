b11fcbf931d57c760be3d4092a72ae19ab12f3b3
antoniorusso2 <russoantonio303@outlook.it>
2024-11-05 16:14:24 +0100

    ADD: 10 list items in page containing different emails

--- js/script.js
+++ js/script.js
@@ -14,25 +14,30 @@ function createDOMElement(tag, classes = [], content = '') {
   return element;
 }
 
-const liElement = createDOMElement('li', 'email');
-console.log;
-ulElement.appendChild(liElement);
-
 //funzione genera numero di mail in base al numero in parametro
+function emailGen(num) {
+  for (let i = 0; i < num; i++) {
 
-for (let i = 0; i < 10; i++) {
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((res) => {
 
-  axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((res) => {
+        const email = res.data.response;
 
-      const email = res.data.response;
+        //creazione elemento lista da mostrare nel dom
+        const liElement = createDOMElement('li', 'email', email);
+        console.log;
+        ulElement.appendChild(liElement);
 
-      console.log(email);
+        // console.log(email);
 
-    })
+      })
 
-    .catch((error) => {
-      console.error('errore');
-    });
+      .catch((error) => {
+        console.error('errore');
+      });
+  }
 }
+
+const mailList = emailGen(10);
+
 
