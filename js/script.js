8d4e60110e8548c5ec4a27620e8e6c052ed37527
antoniorusso2 <russoantonio303@outlook.it>
2024-11-05 17:08:36 +0100

    ADD: button for replacing the mails with others

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 console.log('email list');
-
 //DOM ELEMENTS
+
+//lista dove appendere li contenenti le mail generate
 const ulElement = document.querySelector('.list-group');
 console.log(ulElement);
 
+//button per la generazione di 10 nuove mail
+const btn = document.querySelector('.btn.btn-dark');
+
+//funzione creazione elemento da appendere nel dom
 function createDOMElement(tag, classList = [], content = '') {
 
   const element = document.createElement(tag);
@@ -35,9 +40,17 @@ function emailGen(num) {
       .catch((error) => {
         console.error('errore');
       });
+
   }
 }
 
 const mailList = emailGen(10);
+
+//genera altre 10 mail svuotando il contenitore da quelle precedenti
+btn.addEventListener('click', () => {
+  ulElement.innerHTML = '';
+
+  emailGen(10);
+})
 
 
