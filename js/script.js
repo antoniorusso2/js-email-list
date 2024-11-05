f8bc4362b49c013f533875e438deec087d7b97e3
antoniorusso2 <russoantonio303@outlook.it>
2024-11-05 16:02:07 +0100

    ADD: for loop for generate 10 emails given from API and function for create a DOM element

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
 console.log('email list');
 
+//DOM ELEMENTS
+
+//funzione genera numero di mail in base al numero in parametro
+
+
+for (let i = 0; i < 10; i++) {
+
+  axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((res) => {
+
+      const email = res.data.response;
+
+      console.log(email);
+
+    })
+
+    .catch((error) => {
+      console.error('errore');
+    });
+}
+
+
+
+
+
+function createDOMElement(tag, classes = [], content = '') {
+
+  const element = document.createElement('tag');
+
+  element.classList = classes;
+  element.innerHTML = content;
+}
